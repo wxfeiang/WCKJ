071a4b1d1953ae3263ce844929bd34b5928338ca
wxfeiang <wxfeiang@qq.com>
2019-04-08 18:44:14 +0800

    aa

--- xakp/js/index.js
+++ xakp/js/index.js
@@ -84,24 +84,3 @@ function ban() {
 }
 
 ban();
-
-// 咨询
-
-$(".phonebox").hover(
-  function() {
-    $(this)
-      .stop()
-      .animate({ width: "260px" }, 500);
-  },
-  function() {
-    $(this)
-      .stop()
-      .animate({ width: "66px" }, 500);
-  }
-);
-// 置顶
-$(function() {
-  $(".way-top").click(function() {
-    $("html , body").animate({ scrollTop: 0 }, "slow");
-  });
-});
